d637c34421116e279dec7f5e0116fad515d1081f
Ernani da Costa <ernanidacosta@gmail.com>
2019-11-21 01:59:01 -0300

    update test

--- teste_dominio.py
+++ teste_dominio.py
@@ -2,16 +2,9 @@ import unittest
 
 from dominio import Proposicao
 
-class TestProposicao(unittest.TestCase):
-    def setUp(self):
-        self.proposicao = Proposicao('nome', 'ementa de teste')
-
-    def tearDown(self):
-        print('Sempre após cada teste')
-
+class ProposicaoTest(unittest.TestCase):
+        
     def test_protecao_atributos(self):
-        self.proposicao.nome = 'novo nome'
-        self.assertEqual(self.proposicao.nome, 'nome')
-    
-    def test_menciona_termo(self):
-        self.assertTrue(self.proposicao.menciona_termo("testes"))
+        proposicao = Proposicao('nome', 'termos ementa')
+        proposicao.nome = 'novo nome'
+        self.assertEqual('nome', proposicao.nome)
