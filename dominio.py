c227124d06e694440ad218b3e5dce2ecc4cc3af0
Ernani da Costa <ernanidacosta@gmail.com>
2019-11-20 22:19:04 -0300

    first commit

---
+++ dominio.py
@@ -0,0 +1,22 @@
+class Proposicao:
+    def __init__(self, nome, ementa):
+        self._nome = nome
+        self._ementa = ementa
+    
+    def menciona_termo(self, termo):
+        return termo in self._ementa
+
+    def getNome(self):
+        return self._nome
+
+    def getEmenta(self):
+        return self._ementa
+
+    def setNome(self, nome):
+        pass
+
+    def setEmenta(self, ementa):
+        pass
+
+    nome = property(fget=getNome, fset=setNome)
+    ementa = property(fget=getEmenta, fset=setEmenta)
