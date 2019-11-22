552f6ee39aee83d1577e1f63597f0d27714997a4
Ernani da Costa <ernanidacosta@gmail.com>
2019-11-22 19:21:40 -0300

    Update on cria_url

--- cria_url.py
+++ cria_url.py
@@ -1,12 +1,18 @@
 import urllib.request as request
 
+def cria_url(ano, dtInicio, dtFim):
+    url = 'https://www.camara.leg.br//SitCamaraWs/Proposicoes.asmx/ListarProposicoes?sigla=PL&numero=&ano={}&dataApresentacaoIni={}&dataApresentacaoFim={}&parteNomeAutor=&idTipoAutor=&siglaPartidoAutor=&siglaUFAutor=&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao='
+    return url.format(ano, dtInicio, dtFim)
+
 def abre_url(url):
     req = request.urlopen(url)
     if req.getcode() == 200:
         return req.read()
     else:
         return None
+ano = int(input('Digite o ano: '))
+dtInicio = int(input('Data Inicio: '))
+dtFim = int(input('Data Fim: '))
 
-def cria_url(ano, dtInicio, dtFim):
-    url = 'https://www.camara.leg.br//SitCamaraWs/Proposicoes.asmx/ListarProposicoes?sigla=PL&numero=&ano={}&dataApresentacaoIni={}&dataApresentacaoFim={}&parteNomeAutor=&idTipoAutor=&siglaPartidoAutor=&siglaUFAutor=&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao='
-    return url.format(ano, dInicio, dtFim)
+url = cria_url(ano, dtInicio, dtFim)
+print(abre_url(url))
