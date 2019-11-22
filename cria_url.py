01d047be4269d704fc13ba13c5e8e4c7a5b97600
Ernani da Costa <ernanidacosta@gmail.com>
2019-11-22 19:02:23 -0300

    create abre_url

--- cria_url.py
+++ cria_url.py
@@ -1,3 +1,12 @@
+import urllib.request as request
+
+def abre_url(url):
+    req = request.urlopen(url)
+    if req.getcode() == 200:
+        return req.read()
+    else:
+        return None
+
 def cria_url(ano, dtInicio, dtFim):
     url = 'https://www.camara.leg.br//SitCamaraWs/Proposicoes.asmx/ListarProposicoes?sigla=PL&numero=&ano={}&dataApresentacaoIni={}&dataApresentacaoFim={}&parteNomeAutor=&idTipoAutor=&siglaPartidoAutor=&siglaUFAutor=&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao='
     return url.format(ano, dInicio, dtFim)
