d39ebdfa4df505f6e5c192d31cc09f65b2c742fb
Ernani da Costa <ernanidacosta@gmail.com>
2019-11-23 17:07:25 -0300

    bug fixes

--- arquivos.py
+++ arquivos.py
@@ -1,6 +1,7 @@
-from buscador_de_pls import cria_url, abre_url
+import buscador_de_pls
 
 with open('retorno.xml', 'wb') as data:
     response = abre_url(cria_url(2011, '14/11/2011', '16/11/2011'))
     data.write(response)
 with open('retorno.xml', 'rt', encoding='utf-8') as data:
+    percorre(data.read())
